465754f1bb44c6d0e2ba83c30f5e8201476d6afe
wasdas500 <51051681+wasdas500@users.noreply.github.com>
2019-05-26 16:44:03 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './componenets/Header';
+import Feed from './componenets/Feed';
+function App() {
+  return (
+<div className="App">
+  <Header/>
+  <Feed/>
+</div>
+  );
+}
+
+export default App;
